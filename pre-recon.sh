000aab9a4de9c0720c72914d630bddae9db25813
pepcooo <karabinolaf100@gmail.com>
2026-09-13 00:47:57 +0200

    add simple nmap port scanning by utilizing -N option for the script and fix IP printing

--- pre-recon.sh
+++ pre-recon.sh
@@ -27,6 +27,11 @@ while getopts "hvHDN:" opt; do
         N) 
             port_scan=true
             scan_type="$OPTARG"
+            if [[ ! "$scan_type" =~ ^(s|m|f)$ ]]; then
+                echo -e "${YELLOW}Unknown port scan type." 
+                echo -e "Viable options are: ${RESET}s, m, f."
+                exit 1
+            fi
             ;;
         \?) 
             echo "Unknown option"
@@ -37,33 +42,53 @@ done
 shift $((OPTIND-1))
 
 input=$1
+
+
+if [[ -z "$input" ]]; then
+    echo -e "${RED}Empty input.${RESET}"
+    exit 1 
+fi
+
+
 echo "Scanning $input:"
 
 
 if [[ "$domain_check" == true ]]; then
     domain=""
-    IP=""  
+    ip=""  
     if [[ "$input" =~ $IPv4_REG ]]; then
-        IP="$input"
+        ip="$input"
         domain=$(dig -x "$input" +short)
     else
         domain="$input"
-        IP=$(dig "$domain" +short)
+        ip=$(dig "$domain" +short)
     fi
 
     if [[ -z "$domain" ]]; then
         echo -e "${RED}Couldn't find the domain.${RESET}"
 
     elif [[ -z "$ip" ]]; then
-        echo -e "${RED}Couldn't find th IP address.${RESET}"
+        echo -e "${RED}Couldn't find the IP address.${RESET}"
+
     else
-        echo -e "${GREEN}domain name:${RESET} $domain"
-        echo -e "${GREEN}IP:${RESET} $IP"
+        echo -e "${GREEN}Domain name:${RESET} $domain"
+        echo -e "${GREEN}IP:${RESET} $ip"
     fi
 fi 
 
+if [[ "$port_scan" == true ]]; then
 
-NMAP_SCAN1=$(nmap -sS -A -Pn $1)
+    case "$scan_type" in
+        s)
+            nmap_scan=$(nmap -sS -D RND,RND,RND,RND,RND,RND,RND,ME,RND,RND -Pn -T2 $input)
+            ;;
+        m)
+            nmap_scan=$(nmap -sS -Pn $input)
+            ;;
+        f)  
+            nmap_scan=$(nmap -sS -Pn -F $input)
+            ;;
+    esac
 
-
-echo "$NMAP_SCAN1"
+    echo "$nmap_scan"
+fi
